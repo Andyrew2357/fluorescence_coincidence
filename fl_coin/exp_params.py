6d405f25b206e9dcf4f3c7f252bc89db26740f76
Andyrew2357 <121689389+Andyrew2357@users.noreply.github.com>
2023-07-26 14:16:17 -0400

    Add files via upload

---
+++ fl_coin/exp_params.py
@@ -0,0 +1,8 @@
+"""Experimental Parameters"""
+
+XMAP_ENERGY_UNIT=0.02586 # 1 xMAP energy unit (keV)
+XMAP_TIME_UNIT=20. # 1 xMAP time unit (ns)
+ORBITAL_PERIOD=2563.2 # orbital period of the synchrotron (ns)
+CHANNEL_SOLID_ANGLE=() # Solid angles of detector channels
+CHANNEL_ANGULAR_COEFF=() # Angular factors of detector channels
+ATTENUATION_DOC_PATH='' # Path to a CSV describing the effect of attenuation
