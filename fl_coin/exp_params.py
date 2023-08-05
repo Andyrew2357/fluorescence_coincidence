b7e6f48a722336b5065fbd1e330f830aa238f164
Andyrew2357 <121689389+Andyrew2357@users.noreply.github.com>
2023-08-05 12:38:08 -0400

    Update exp_params.py added parameters that were missing

--- fl_coin/exp_params.py
+++ fl_coin/exp_params.py
@@ -3,6 +3,17 @@
 XMAP_ENERGY_UNIT=0.02586 # 1 xMAP energy unit (keV)
 XMAP_TIME_UNIT=20. # 1 xMAP time unit (ns)
 ORBITAL_PERIOD=2563.2 # orbital period of the synchrotron (ns)
-CHANNEL_SOLID_ANGLE=(0.11,0.0935,0.0935,0.0935) # Solid angles of detector channels
-CHANNEL_ANGULAR_COEFF=(0.7,0.6,0.6,0.6) # Angular factors of detector channels
-ATTENUATION_DOC_PATH='' # Path to a CSV describing the effect of attenuation
+CHANNEL_SOLID_ANGLE=(0.11,0.0935,0.0935,0.0935) # solid angles of detector channels
+CHANNEL_ANGULAR_COEFF=(0.7,0.6,0.6,0.6) # angular factors of detector channels
+ATTENUATION_DOC_PATH='' # path to a CSV describing the effect of attenuation
+E_FLUOR=8. # fluorescence energy used for the purpose of attentuation correction (keV)
+
+"""Prediction Parameters"""
+
+K_COEFF=1.4e-6 
+D=4600
+EB=8.979
+E0=46
+Z=29
+MC2=511
+ALPHA=1/137
